ad0c5e95928f1ee5dd49a839e6bc464a4c40ff0f
teuncm <teun.c.mathijssen@gmail.com>
2026-01-31 15:58:08 +0100

    test

---
+++ src/teuncmtester/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2026-present teuncm <teun.c.mathijssen@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.2"
---
+++ src/teuncmtester/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2026-present teuncm <teun.c.mathijssen@gmail.com>
+#
+# SPDX-License-Identifier: MIT
